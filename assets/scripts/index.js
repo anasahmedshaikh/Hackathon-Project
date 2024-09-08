1c17014466db2133dcec57143b9e041beec1306d
anasahmedshaikh <156065391+anasahmedshaikh@users.noreply.github.com>
2024-09-08 06:37:11 +0500

    milestone 3 : achieved the functionality to create a resume

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,188 @@
-"use strict";
+// Regular Expressions for validation
+var strRegex = /^[a-zA-Z\s]*$/; // Only letters and spaces
+var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+var phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+var digitRegex = /^\d+$/; // Only digits
+// HTML Form Elements
+var mainForm = document.getElementById('cv-form');
+// Validation types enum
+var ValidType;
+(function (ValidType) {
+    ValidType["TEXT"] = "text";
+    ValidType["TEXT_EMP"] = "text_emp";
+    ValidType["EMAIL"] = "email";
+    ValidType["DIGIT"] = "digit";
+    ValidType["PHONENO"] = "phoneno";
+    ValidType["ANY"] = "any";
+})(ValidType || (ValidType = {}));
+// User input elements
+var firstnameElem = mainForm === null || mainForm === void 0 ? void 0 : mainForm.firstname;
+var middlenameElem = mainForm === null || mainForm === void 0 ? void 0 : mainForm.middlename;
+var lastnameElem = mainForm === null || mainForm === void 0 ? void 0 : mainForm.lastname;
+var imageElem = mainForm === null || mainForm === void 0 ? void 0 : mainForm.image;
+var designationElem = mainForm === null || mainForm === void 0 ? void 0 : mainForm.designation;
+var addressElem = mainForm === null || mainForm === void 0 ? void 0 : mainForm.address;
+var emailElem = mainForm === null || mainForm === void 0 ? void 0 : mainForm.email;
+var phonenoElem = mainForm === null || mainForm === void 0 ? void 0 : mainForm.phoneno;
+var summaryElem = mainForm === null || mainForm === void 0 ? void 0 : mainForm.summary;
+// Display elements
+var nameDsp = document.getElementById('fullname_dsp');
+var imageDsp = document.getElementById('image_dsp');
+var phonenoDsp = document.getElementById('phoneno_dsp');
+var emailDsp = document.getElementById('email_dsp');
+var addressDsp = document.getElementById('address_dsp');
+var designationDsp = document.getElementById('designation_dsp');
+var summaryDsp = document.getElementById('summary_dsp');
+var projectsDsp = document.getElementById('projects_dsp');
+var achievementsDsp = document.getElementById('achievements_dsp');
+var skillsDsp = document.getElementById('skills_dsp');
+var educationsDsp = document.getElementById('educations_dsp');
+var experiencesDsp = document.getElementById('experiences_dsp');
+// Function to fetch values
+var fetchValues = function (attrs) {
+    var nodeLists = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        nodeLists[_i - 1] = arguments[_i];
+    }
+    var elemsAttrsCount = nodeLists.length;
+    var elemsDataCount = nodeLists[0].length;
+    var tempDataArr = [];
+    for (var i = 0; i < elemsDataCount; i++) {
+        var dataObj = {};
+        for (var j = 0; j < elemsAttrsCount; j++) {
+            dataObj["".concat(attrs[j])] = nodeLists[j][i].value;
+        }
+        tempDataArr.push(dataObj);
+    }
+    return tempDataArr;
+};
+// Function to get user inputs
+var getUserInputs = function () {
+    var achievementsTitleElem = document.querySelectorAll('.achieve_title');
+    var achievementsDescriptionElem = document.querySelectorAll('.achieve_description');
+    var expTitleElem = document.querySelectorAll('.exp_title');
+    var expOrganizationElem = document.querySelectorAll('.exp_organization');
+    var expLocationElem = document.querySelectorAll('.exp_location');
+    var expStartDateElem = document.querySelectorAll('.exp_start_date');
+    var expEndDateElem = document.querySelectorAll('.exp_end_date');
+    var expDescriptionElem = document.querySelectorAll('.exp_description');
+    var eduSchoolElem = document.querySelectorAll('.edu_school');
+    var eduDegreeElem = document.querySelectorAll('.edu_degree');
+    var eduCityElem = document.querySelectorAll('.edu_city');
+    var eduStartDateElem = document.querySelectorAll('.edu_start_date');
+    var eduGraduationDateElem = document.querySelectorAll('.edu_graduation_date');
+    var eduDescriptionElem = document.querySelectorAll('.edu_description');
+    var projTitleElem = document.querySelectorAll('.proj_title');
+    var projLinkElem = document.querySelectorAll('.proj_link');
+    var projDescriptionElem = document.querySelectorAll('.proj_description');
+    var skillElem = document.querySelectorAll('.skill');
+    return {
+        firstname: firstnameElem === null || firstnameElem === void 0 ? void 0 : firstnameElem.value,
+        middlename: middlenameElem === null || middlenameElem === void 0 ? void 0 : middlenameElem.value,
+        lastname: lastnameElem === null || lastnameElem === void 0 ? void 0 : lastnameElem.value,
+        designation: designationElem === null || designationElem === void 0 ? void 0 : designationElem.value,
+        address: addressElem === null || addressElem === void 0 ? void 0 : addressElem.value,
+        email: emailElem === null || emailElem === void 0 ? void 0 : emailElem.value,
+        phoneno: phonenoElem === null || phonenoElem === void 0 ? void 0 : phonenoElem.value,
+        summary: summaryElem === null || summaryElem === void 0 ? void 0 : summaryElem.value,
+        achievements: fetchValues(['achieve_title', 'achieve_description'], achievementsTitleElem, achievementsDescriptionElem),
+        experiences: fetchValues(['exp_title', 'exp_organization', 'exp_location', 'exp_start_date', 'exp_end_date', 'exp_description'], expTitleElem, expOrganizationElem, expLocationElem, expStartDateElem, expEndDateElem, expDescriptionElem),
+        educations: fetchValues(['edu_school', 'edu_degree', 'edu_city', 'edu_start_date', 'edu_graduation_date', 'edu_description'], eduSchoolElem, eduDegreeElem, eduCityElem, eduStartDateElem, eduGraduationDateElem, eduDescriptionElem),
+        projects: fetchValues(['proj_title', 'proj_link', 'proj_description'], projTitleElem, projLinkElem, projDescriptionElem),
+        skills: fetchValues(['skill'], skillElem),
+    };
+};
+// Form validation
+var validateFormData = function (elem, elemType, elemName) {
+    var value = elem.value.trim();
+    var isValid = true;
+    if (elemType === ValidType.TEXT || elemType === ValidType.TEXT_EMP) {
+        isValid = strRegex.test(value) && (elemType === ValidType.TEXT ? value.length > 0 : true);
+    }
+    else if (elemType === ValidType.EMAIL) {
+        isValid = emailRegex.test(value);
+    }
+    else if (elemType === ValidType.PHONENO) {
+        isValid = phoneRegex.test(value);
+    }
+    else if (elemType === ValidType.ANY) {
+        isValid = value.length > 0;
+    }
+    if (!isValid) {
+        addErrMsg(elem, elemName);
+    }
+    else {
+        removeErrMsg(elem);
+    }
+};
+// Error message handlers
+var addErrMsg = function (formElem, formElemName) {
+    var nextElem = formElem.nextElementSibling;
+    if (nextElem) {
+        nextElem.innerHTML = "".concat(formElemName, " is invalid");
+    }
+};
+var removeErrMsg = function (formElem) {
+    var nextElem = formElem.nextElementSibling;
+    if (nextElem) {
+        nextElem.innerHTML = '';
+    }
+};
+// Show the list data
+var showListData = function (listData, listContainer) {
+    listContainer.innerHTML = '';
+    listData.forEach(function (listItem) {
+        var itemElem = document.createElement('div');
+        itemElem.classList.add('preview-item');
+        for (var key in listItem) {
+            var subItemElem = document.createElement('span');
+            subItemElem.classList.add('preview-item-val');
+            subItemElem.innerHTML = "".concat(listItem[key]);
+            itemElem.appendChild(subItemElem);
+        }
+        listContainer.appendChild(itemElem);
+    });
+};
+// Display the CV
+var displayCV = function (userData) {
+    nameDsp.innerHTML = "".concat(userData.firstname, " ").concat(userData.middlename, " ").concat(userData.lastname);
+    phonenoDsp.innerHTML = userData.phoneno;
+    emailDsp.innerHTML = userData.email;
+    addressDsp.innerHTML = userData.address;
+    designationDsp.innerHTML = userData.designation;
+    summaryDsp.innerHTML = userData.summary;
+    showListData(userData.projects, projectsDsp);
+    showListData(userData.achievements, achievementsDsp);
+    showListData(userData.skills, skillsDsp);
+    showListData(userData.educations, educationsDsp);
+    showListData(userData.experiences, experiencesDsp);
+};
+// generate CV
+var generateCV = function () {
+    var userData = getUserInputs();
+    displayCV(userData);
+    console.log(userData);
+};
+// function previewImage(){
+//     let oFReader = new FileReader();
+//     oFReader.readAsDataURL(imageElem.files[0]);
+//     oFReader.onload = function(ofEvent){
+//         imageDsp.src = ofEvent.target.result;
+//     }
+// }
+function previewImage() {
+    var imageElem = document.getElementById("imageElem.files[0]");
+    var imageDsp = document.getElementById('yourImageDisplayId');
+    if (imageElem && imageElem.files && imageElem.files[0]) {
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(imageElem.files[0]);
+        oFReader.onload = function (ofEvent) {
+            if (ofEvent.target) {
+                imageDsp.src = ofEvent.target.result;
+            }
+        };
+    }
+}
 // print CV
 function printCV() {
     window.print();
--- assets/scripts/index.ts
+++ assets/scripts/index.ts
@@ -1,3 +1,206 @@
+// Regular Expressions for validation
+const strRegex: RegExp = /^[a-zA-Z\s]*$/; // Only letters and spaces
+const emailRegex: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const phoneRegex: RegExp = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+const digitRegex: RegExp = /^\d+$/; // Only digits
+
+// HTML Form Elements
+const mainForm = document.getElementById('cv-form') as HTMLFormElement | null;
+
+// Validation types enum
+enum ValidType {
+    TEXT = 'text',
+    TEXT_EMP = 'text_emp',
+    EMAIL = 'email',
+    DIGIT = 'digit',
+    PHONENO = 'phoneno',
+    ANY = 'any',
+}
+
+// User input elements
+const firstnameElem = mainForm?.firstname as HTMLInputElement;
+const middlenameElem = mainForm?.middlename as HTMLInputElement;
+const lastnameElem = mainForm?.lastname as HTMLInputElement;
+const imageElem = mainForm?.image as HTMLInputElement;
+const designationElem = mainForm?.designation as HTMLInputElement;
+const addressElem = mainForm?.address as HTMLInputElement;
+const emailElem = mainForm?.email as HTMLInputElement;
+const phonenoElem = mainForm?.phoneno as HTMLInputElement;
+const summaryElem = mainForm?.summary as HTMLTextAreaElement;
+
+// Display elements
+const nameDsp = document.getElementById('fullname_dsp') as HTMLElement;
+const imageDsp = document.getElementById('image_dsp') as HTMLImageElement;
+const phonenoDsp = document.getElementById('phoneno_dsp') as HTMLElement;
+const emailDsp = document.getElementById('email_dsp') as HTMLElement;
+const addressDsp = document.getElementById('address_dsp') as HTMLElement;
+const designationDsp = document.getElementById('designation_dsp') as HTMLElement;
+const summaryDsp = document.getElementById('summary_dsp') as HTMLElement;
+const projectsDsp = document.getElementById('projects_dsp') as HTMLElement;
+const achievementsDsp = document.getElementById('achievements_dsp') as HTMLElement;
+const skillsDsp = document.getElementById('skills_dsp') as HTMLElement;
+const educationsDsp = document.getElementById('educations_dsp') as HTMLElement;
+const experiencesDsp = document.getElementById('experiences_dsp') as HTMLElement;
+
+// Function to fetch values
+const fetchValues = (attrs: string[], ...nodeLists: NodeListOf<HTMLInputElement | HTMLTextAreaElement>[]): object[] => {
+    const elemsAttrsCount = nodeLists.length;
+    const elemsDataCount = nodeLists[0].length;
+    let tempDataArr: object[] = [];
+
+    for (let i = 0; i < elemsDataCount; i++) {
+        let dataObj: { [key: string]: string } = {};
+        for (let j = 0; j < elemsAttrsCount; j++) {
+            dataObj[`${attrs[j]}`] = nodeLists[j][i].value;
+        }
+        tempDataArr.push(dataObj);
+    }
+
+    return tempDataArr;
+};
+
+// Function to get user inputs
+const getUserInputs = (): object => {
+    const achievementsTitleElem = document.querySelectorAll('.achieve_title') as NodeListOf<HTMLInputElement>;
+    const achievementsDescriptionElem = document.querySelectorAll('.achieve_description') as NodeListOf<HTMLTextAreaElement>;
+
+    const expTitleElem = document.querySelectorAll('.exp_title') as NodeListOf<HTMLInputElement>;
+    const expOrganizationElem = document.querySelectorAll('.exp_organization') as NodeListOf<HTMLInputElement>;
+    const expLocationElem = document.querySelectorAll('.exp_location') as NodeListOf<HTMLInputElement>;
+    const expStartDateElem = document.querySelectorAll('.exp_start_date') as NodeListOf<HTMLInputElement>;
+    const expEndDateElem = document.querySelectorAll('.exp_end_date') as NodeListOf<HTMLInputElement>;
+    const expDescriptionElem = document.querySelectorAll('.exp_description') as NodeListOf<HTMLTextAreaElement>;
+
+    const eduSchoolElem = document.querySelectorAll('.edu_school') as NodeListOf<HTMLInputElement>;
+    const eduDegreeElem = document.querySelectorAll('.edu_degree') as NodeListOf<HTMLInputElement>;
+    const eduCityElem = document.querySelectorAll('.edu_city') as NodeListOf<HTMLInputElement>;
+    const eduStartDateElem = document.querySelectorAll('.edu_start_date') as NodeListOf<HTMLInputElement>;
+    const eduGraduationDateElem = document.querySelectorAll('.edu_graduation_date') as NodeListOf<HTMLInputElement>;
+    const eduDescriptionElem = document.querySelectorAll('.edu_description') as NodeListOf<HTMLTextAreaElement>;
+
+    const projTitleElem = document.querySelectorAll('.proj_title') as NodeListOf<HTMLInputElement>;
+    const projLinkElem = document.querySelectorAll('.proj_link') as NodeListOf<HTMLInputElement>;
+    const projDescriptionElem = document.querySelectorAll('.proj_description') as NodeListOf<HTMLTextAreaElement>;
+
+    const skillElem = document.querySelectorAll('.skill') as NodeListOf<HTMLInputElement>;
+
+    return {
+        firstname: firstnameElem?.value ,
+        middlename: middlenameElem?.value,
+        lastname: lastnameElem?.value,
+        designation: designationElem?.value,
+        address: addressElem?.value,
+        email: emailElem?.value,
+        phoneno: phonenoElem?.value,
+        summary: summaryElem?.value,
+        achievements: fetchValues(['achieve_title', 'achieve_description'], achievementsTitleElem, achievementsDescriptionElem),
+        experiences: fetchValues(['exp_title', 'exp_organization', 'exp_location', 'exp_start_date', 'exp_end_date', 'exp_description'], expTitleElem, expOrganizationElem, expLocationElem, expStartDateElem, expEndDateElem, expDescriptionElem),
+        educations: fetchValues(['edu_school', 'edu_degree', 'edu_city', 'edu_start_date', 'edu_graduation_date', 'edu_description'], eduSchoolElem, eduDegreeElem, eduCityElem, eduStartDateElem, eduGraduationDateElem, eduDescriptionElem),
+        projects: fetchValues(['proj_title', 'proj_link', 'proj_description'], projTitleElem, projLinkElem, projDescriptionElem),
+        skills: fetchValues(['skill'], skillElem),
+    };
+};
+
+// Form validation
+const validateFormData = (elem: HTMLInputElement | HTMLTextAreaElement, elemType: ValidType, elemName: string) => {
+    const value = elem.value.trim();
+    let isValid = true;
+
+    if (elemType === ValidType.TEXT || elemType === ValidType.TEXT_EMP) {
+        isValid = strRegex.test(value) && (elemType === ValidType.TEXT ? value.length > 0 : true);
+    } else if (elemType === ValidType.EMAIL) {
+        isValid = emailRegex.test(value);
+    } else if (elemType === ValidType.PHONENO) {
+        isValid = phoneRegex.test(value);
+    } else if (elemType === ValidType.ANY) {
+        isValid = value.length > 0;
+    }
+
+    if (!isValid) {
+        addErrMsg(elem, elemName);
+    } else {
+        removeErrMsg(elem);
+    }
+};
+
+// Error message handlers
+const addErrMsg = (formElem: HTMLInputElement | HTMLTextAreaElement, formElemName: string) => {
+    const nextElem = formElem.nextElementSibling as HTMLElement | null;
+    if (nextElem) {
+        nextElem.innerHTML = `${formElemName} is invalid`;
+    }
+};
+
+const removeErrMsg = (formElem: HTMLInputElement | HTMLTextAreaElement) => {
+    const nextElem = formElem.nextElementSibling as HTMLElement | null;
+    if (nextElem) {
+        nextElem.innerHTML = '';
+    }
+};
+
+// Show the list data
+const showListData = (listData: object[], listContainer: HTMLElement) => {
+    listContainer.innerHTML = '';
+    listData.forEach(listItem => {
+        const itemElem = document.createElement('div');
+        itemElem.classList.add('preview-item');
+
+        for (const key in listItem) {
+            const subItemElem = document.createElement('span');
+            subItemElem.classList.add('preview-item-val');
+            subItemElem.innerHTML = `${listItem[key]}`;
+            itemElem.appendChild(subItemElem);
+        }
+
+        listContainer.appendChild(itemElem);
+    });
+};
+
+// Display the CV
+const displayCV = (userData: any) => {
+    nameDsp.innerHTML = `${userData.firstname} ${userData.middlename} ${userData.lastname}`;
+    phonenoDsp.innerHTML = userData.phoneno;
+    emailDsp.innerHTML = userData.email;
+    addressDsp.innerHTML = userData.address;
+    designationDsp.innerHTML = userData.designation;
+    summaryDsp.innerHTML = userData.summary;
+    showListData(userData.projects, projectsDsp);
+    showListData(userData.achievements, achievementsDsp);
+    showListData(userData.skills, skillsDsp);
+    showListData(userData.educations, educationsDsp);
+    showListData(userData.experiences, experiencesDsp);
+};
+
+// generate CV
+const generateCV = () => {
+    let userData = getUserInputs();
+    displayCV(userData);
+    console.log(userData);
+}
+
+// function previewImage(){
+//     let oFReader = new FileReader();
+//     oFReader.readAsDataURL(imageElem.files[0]);
+//     oFReader.onload = function(ofEvent){
+//         imageDsp.src = ofEvent.target.result;
+//     }
+// }
+function previewImage(): void {
+    const imageElem = document.getElementById("imageElem.files[0]") as HTMLInputElement;
+    const imageDsp = document.getElementById('yourImageDisplayId') as HTMLImageElement;
+
+    if (imageElem && imageElem.files && imageElem.files[0]) {
+        const oFReader = new FileReader();
+        oFReader.readAsDataURL(imageElem.files[0]);
+
+        oFReader.onload = function (ofEvent: ProgressEvent<FileReader>) {
+            if (ofEvent.target) {
+                imageDsp.src = ofEvent.target.result as string;
+            }
+        };
+    }
+}
+
 // print CV
 function printCV(){
     window.print();
